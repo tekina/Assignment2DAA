5cef89c6a4f318784d52189edda6279dc05819b3
Aniket <aniketgarg@hotmail.com>
2014-04-24 01:49:21 +0530

    added method to find min-cut and measure total running time of algorithm

--- src/FF.java
+++ src/FF.java
@@ -1,17 +1,19 @@
-import java.io.EOFException;
 import java.io.File;
 import java.io.FileNotFoundException;
 import java.util.Arrays;
 import java.util.Scanner;
 
 public class FF {
+	
+	private static long startTime = System.currentTimeMillis();
 
-	public static int SIZE = 6;
-	int[][] edge = new int[SIZE][SIZE];
-	int[][] flow = new int[SIZE][SIZE];
-	boolean isVisited[] = new boolean[SIZE];
+	private int SIZE;
+	int[][] edge;
+	int[][] flow;
+	boolean isVisited[];
+	int[][] minCutEdge;
 
-	int[] augPath = new int[SIZE];
+	int[] augPath;
 	int ctr;
 	int src = 0;
 	int sink = 5;
@@ -19,7 +21,7 @@ public class FF {
 
 	public static void main(String[] args) {
 		File fin;
-		fin = new File(args[0]);
+		fin = new File("res/edges.txt");
 		FF d1 = new FF();
 		Scanner sc;
 		try {
@@ -36,43 +38,41 @@ public class FF {
 		int sent;
 		while ((sent = d1.send(d1.src, d1.sink, Integer.MAX_VALUE)) > 0) {
 			d1.netFlow += sent;
+		//	d1.printMatrix();
+			System.out.println("Working...");
 			Arrays.fill(d1.isVisited, false);
 		}
-		d1.printMatrix();
-
+	//	d1.printMatrix();
+		System.out.println("\nNet Flow = " + d1.netFlow);
+		d1.minCut();
+		long endTime = System.currentTimeMillis();
+        System.out.println("It took " + (endTime - startTime) + " milliseconds");
 	}
 
 	/**
-	 * populate the adjacency matrix with edges from file whose path is supplied
-	 * to Scanner
+	 * populate the adjacency matrix with edges from file which resides in /res
+	 * folder of the project and whose path is supplied to Scanner
 	 */
 	void populateEdges(Scanner sc) {
 		int from, to, cap;
 		src = 0;
 		SIZE = sc.nextInt();
 		sink = SIZE - 1;
+		isVisited = new boolean[SIZE];
+		flow = new int[SIZE][SIZE];
+		edge = new int[SIZE][SIZE];
+		augPath = new int[SIZE];
+		minCutEdge = new int[SIZE][2];
 		sc.nextInt();
 		try {
 			while (true) {
 				from = sc.nextInt();
 				to = sc.nextInt();
 				cap = sc.nextInt();
-				edge[from-1][to-1] = cap;
+				edge[from - 1][to - 1] = cap;
 			}
-		} catch (Exception e) {
+		} catch (Exception e) { // exception is used to handle EOF.
 			return;
-		}
-	}
-
-	/**
-	 * Display adjacency matrix
-	 */
-	void printEdges() {
-		for (int i = 0; i < SIZE; i++) {
-			for (int j = 0; j < SIZE; j++) {
-				System.out.print(edge[i][j] + " ");
-			}
-			System.out.println();
 		}
 	}
 
@@ -80,9 +80,6 @@ public class FF {
 	 * Prints the flow matrix, residual graph matrix and original matrix
 	 */
 	void printMatrix() {
-		for (int i = 0; i < SIZE; i++) {
-			System.out.println(augPath[i]);
-		}
 		System.out.println("Printing flow array");
 
 		for (int i = 0; i < SIZE; i++) {
@@ -107,9 +104,6 @@ public class FF {
 			}
 			System.out.println();
 		}
-
-		System.out.println("\nNet Flow = " + netFlow);
-
 	}
 
 	/**
@@ -143,6 +137,38 @@ public class FF {
 
 	int min(int a, int b) {
 		return a < b ? a : b;
+	}
+
+	void minCut() {
+		ctr = 0;
+		Arrays.fill(isVisited, false);
+		for (int i = 0; i < SIZE; i++) {
+			if (edge[src][i] > 0) {
+				doDFS(src);
+			}
+		}
+		for (int i = 0; i < ctr; i++) {
+			System.out
+					.println("\n" + minCutEdge[i][0] + " " + minCutEdge[i][1]);
+		}
+	}
+
+	void doDFS(int currNode) {
+		isVisited[currNode] = true;
+		for (int i = 1; i < SIZE; i++) {
+			if (edge[currNode][i] - flow[currNode][i] > 0
+					&& isVisited[i] == false) {
+				isVisited[i] = true;
+				doDFS(i);
+			} else if (edge[currNode][i] - flow[currNode][i] == 0
+					&& edge[currNode][i] != 0 && isVisited[i] == false) {
+				minCutEdge[ctr][0] = currNode;
+				minCutEdge[ctr][1] = i;
+				isVisited[i] = true;
+				ctr++;
+				return;
+			}
+		}
 	}
 
 }
